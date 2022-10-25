cb7fb2579046a7f56944fc0f7070be27316b2745
ianlee02 <>
2022-10-25 10:18:28 -0500

    Added starting questions

--- jsFiles/00_helloworld.js
+++ jsFiles/00_helloworld.js
@@ -1,24 +1,24 @@
-colorVal = 0;
 totalPoints = 0;
 lives = 3;
 function setup() {
     createCanvas(windowWidth, windowHeight);
 }
 
-function changeBackground() {
-    if (colorVal % 3 == 0) {
-        document.body.style.background = "red"
-    }
-    if (colorVal % 3 == 1) {
-        document.body.style.background = "green"
-    }
-    if (colorVal % 3 == 2) {
-        document.body.style.background = "blue"
-    }
-    colorVal++;
+function changeBackground(points) {
+    document.body.style.background = "green"
     console.log("Mouse clicked")
+    totalPoints = totalPoints + points;
+    console.log(totalPoints)
+    console.log(lives)
 }
 
-function draw() {
+function wrongAnswer() {
+    document.body.style.background = "red"
+    lives--;
+    console.log(totalPoints)
+    console.log(lives)
+}
 
+function formToMemory() {
+    
 }
