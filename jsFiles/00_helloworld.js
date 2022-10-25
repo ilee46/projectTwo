ce6e0fc712dbcd670f0203c55470c7214bbda6fd
ianlee02 <>
2022-10-25 08:53:22 -0500

    Added starting code

--- jsFiles/00_helloworld.js
+++ jsFiles/00_helloworld.js
@@ -1,9 +1,21 @@
+colorVal = 0;
+totalPoints = 0;
+lives = 3;
 function setup() {
     createCanvas(windowWidth, windowHeight);
 }
 
 function changeBackground() {
-    document.body.style.background = "cyan"
+    if (colorVal % 3 == 0) {
+        document.body.style.background = "red"
+    }
+    if (colorVal % 3 == 1) {
+        document.body.style.background = "green"
+    }
+    if (colorVal % 3 == 2) {
+        document.body.style.background = "blue"
+    }
+    colorVal++;
     console.log("Mouse clicked")
 }
 
